a595abb3df34e303e26c894075337f71a7d1105e
Chijioke Ogbuanya <53488449+EngrGord@users.noreply.github.com>
2022-09-09 14:42:21 +0100

    Update FunctionForm.jsx

--- packages/react-app/src/components/Contract/FunctionForm.jsx
+++ packages/react-app/src/components/Contract/FunctionForm.jsx
@@ -33,9 +33,6 @@ export default function FunctionForm({ contractFunction, functionInfo, provider,
 
   const inputs = functionInfo.inputs.map((input, inputIndex) => {
     const key = getFunctionInputKey(functionInfo, input, inputIndex);
-    console.log(functionInfo);
-    console.log(input);
-    console.log(inputIndex);
 
     let buttons = "";
     if (input.type === "bytes32") {
