a48580efaa26a2ea215bc87a56274d5883e4b4d7
Chris <chijokeogbuanya@gmail.com>
2022-09-30 10:22:52 +0000

    ERC20 lessons

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -2,26 +2,60 @@ pragma solidity >=0.8.0 <0.9.0;
 //SPDX-License-Identifier: MIT
 
 import "hardhat/console.sol";
-// import "@openzeppelin/contracts/access/Ownable.sol"; 
-// https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
 
-contract YourContract {
 
-  event SetPurpose(address sender, string purpose);
+interface IERC20 {
 
-  string public purpose = "Building Unstoppable Apps!!!";
+  event Transfer(address indexed from, address indexed to, uint amount);
+  event Approval(address indexed owner, address indexed spender, uint amount);
 
-  constructor() payable {
-    // what should we do on deploy?
+  function totalSupply() external view returns (uint);
+  function balanceOf(address account) external view returns (uint);
+  function transfer(address recipient, uint amount) external returns (bool);
+  function allowance(address owner, address spender) external view returns (uint);
+  function approve(address spender, uint amount) external returns (bool);
+  function transferFrom(address spender, address recipient, uint amount) external returns (bool);
+}
+
+contract YourContract is IERC20 {
+  uint public override totalSupply;
+  mapping(address => uint) public override balanceOf;
+  mapping(address => mapping(address => uint)) public override allowance;
+  string public name = "Test";
+  string public symbol = "TEST";
+  uint8 public decimals = 18;
+
+  function transfer(address recipient, uint amount) external override returns (bool){
+    balanceOf[msg.sender] -= amount;
+    balanceOf[recipient] += amount;
+    emit Transfer(msg.sender, recipient, amount);
+    return true;
+  }
+  function approve(address spender, uint amount) external override returns (bool){
+    allowance[msg.sender][spender] = amount;
+    emit Approval(msg.sender, spender, amount);
+    return true;
+  }
+  function transferFrom(address sender, address recipient, uint amount) external override returns (bool) {
+    allowance[sender][msg.sender] -= amount;
+    balanceOf[sender] -= amount;
+    balanceOf[recipient] += amount;
+    emit Transfer(sender, recipient, amount);
+    return true;
   }
 
-  function setPurpose(string memory newPurpose) public {
-      purpose = newPurpose;
-      console.log(msg.sender,"set purpose to",purpose);
-      emit SetPurpose(msg.sender, purpose);
+  function mint(uint amount) external {
+    balanceOf[msg.sender] += amount;
+    totalSupply += amount;
+    emit Transfer(address(0), msg.sender, amount);
   }
 
-  // to support receiving ETH by default
-  receive() external payable {}
-  fallback() external payable {}
+  function burn(uint amount) external {
+    balanceOf[msg.sender] -= amount;
+    totalSupply -= amount;
+    emit Transfer(msg.sender, address(0), amount);
+  }
+  
+  
+
 }
