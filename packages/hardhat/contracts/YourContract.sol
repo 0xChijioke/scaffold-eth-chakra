2e5f4e175828ceebd292c95fa29faa0bf2079101
Chris <chijokeogbuanya@gmail.com>
2022-10-06 23:53:50 +0000

    Staking contract

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -1,27 +1,102 @@
 pragma solidity >=0.8.0 <0.9.0;
 //SPDX-License-Identifier: MIT
 
-import "hardhat/console.sol";
-// import "@openzeppelin/contracts/access/Ownable.sol"; 
-// https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
+interface IERC20 {
 
-contract YourContract {
+  event Transfer(address indexed from, address indexed to, uint amount);
+  event Approval(address indexed owner, address indexed spender, uint amount);
 
-  event SetPurpose(address sender, string purpose);
+  function totalSupply() external view returns (uint);
+  function balanceOf(address account) external view returns (uint);
+  function transfer(address recipient, uint amount) external returns (bool);
+  function allowance(address owner, address spender) external view returns (uint);
+  function approve(address spender, uint amount) external returns (bool);
+  function transferFrom(address spender, address recipient, uint amount) external returns (bool);
+}
 
-  string public purpose = "Building Unstoppable Apps!!!";
+contract StakingRewards {
+  IERC20 public immutable stakingToken;
+  IERC20 public immutable rewardToken;
 
-  constructor() payable {
-    // what should we do on deploy?
+  address public owner;
+
+  uint public duration;
+  uint public finishedAt;
+  uint public updatedAt;
+  uint public rewardRate;
+  uint public rewardPerTokenStored;
+  mapping(address => uint) public userRewardPerTokenStored;
+  mapping(address => uint) public rewards;
+
+  uint public totalSupply;
+  mapping(address => uint) public balanceOf;
+
+  modifier onlyOwner() {
+    require(msg.sender == owner, "Not the owner!");
+    _;
   }
 
-  function setPurpose(string memory newPurpose) public {
-      purpose = newPurpose;
-      console.log(msg.sender,"set purpose to",purpose);
-      emit SetPurpose(msg.sender, purpose);
+  constructor(address _stakingToken, address _rewardToken) {
+    owner = msg.sender;
+    stakingToken = IERC20(_stakingToken);
+    rewardToken = IERC20(_rewardToken);
   }
 
-  // to support receiving ETH by default
-  receive() external payable {}
-  fallback() external payable {}
+
+  function setRewardsDuration(uint _duration) external onlyOwner {
+    require(finishedAt < block.timestamp, "Reward duration not finished");
+    duration = _duration;
+  }
+
+  function notifyRewardAmount(uint _amount) external onlyOwner {
+    if (block.timestamp > finishedAt){
+      rewardRate = _amount;
+    } else {
+      uint remainingRewards = rewardRate * (finishedAt - block.timestamp);
+      rewardRate = (remainingRewards + _amount) / duration;
+
+    }
+      require(rewardRate > 0, "RewardRate = 0");
+      require(rewardRate * duration <= rewardToken.balanceOf(address(this)));
+
+      finishedAt = duration + block.timestamp;
+      updatedAt = block.timestamp;
+  }
+
+
+  function stake(uint _amount) external {
+    require(_amount > 0, "Amount = 0");
+    stakingToken.transferFrom(msg.sender, address(this), _amount);
+    balanceOf[msg.sender] = _amount;
+    totalSupply += _amount;
+  }
+
+  function withdraw(uint _amount) external {
+    require(_amount > 0, "Amount = 0");
+    balanceOf[msg.sender] -= _amount;
+    totalSupply -= _amount;
+    stakingToken.transfer(msg.sender, _amount);
+  }
+
+  function lastTimeRewardApplicable() public view returns (uint) {
+    min(block.timestamp, finishedAt);
+  }
+
+  function rewardPerToken() public view returns (uint) {
+    if (totalSupply == 0) {
+      return rewardPerTokenStored;
+    }
+    return rewardPerTokenStored + (rewardRate * (lastTimeRewardApplicable() - updatedAt) * 1e18) / totalSupply;
+  }
+
+  function earned(address _account) external view returns (uint) {
+    balanceOf[_account] * ((rewardPerToken() - userRewardPerTokenStored[_account]) / 1e18) + rewards[_account];
+  }
+
+  function getReward() external {}
+
+  function min(uint x, uint y) private pure returns (uint) {
+    return x <= y ? x : y;
+  }
+
 }
